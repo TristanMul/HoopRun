7ef38276da793df175b4140d54e2d0eb5408ae77
Tristan <tristanmul@gmail.com>
2021-04-23 15:05:45 +0200

    Organised project

---
+++ Hoops Race/Assets/Scripts/Obstacles/IceShard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceShard : MonoBehaviour
+{
+    public bool iceBroken;
+    private bool shardBroken;
+    //&& (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (iceBroken && !shardBroken)
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            shardBroken = true;
+        }
+    }
+}
